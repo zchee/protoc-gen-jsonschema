618dd05e044280ba0b752e1d0cdc74cbc93799dd
Koichi Shiraishi <zchee.io@gmail.com>
2019-02-23 12:48:10 +0900

    hack/make: update go.mk to latest

--- hack/make/go.mk
+++ hack/make/go.mk
@@ -2,34 +2,20 @@
 # global
 
 SHELL = /usr/bin/env bash
-GO_PATH = $(shell go env GOPATH)
+
+GO_PATH ?= $(shell go env GOPATH)
+GO_OS ?= $(shell go env GOOS)
+GO_ARCH ?= $(shell go env GOARCH)
+GOLANG_VERSION ?= 1.11
+ALPINE_VERSION ?= 3.9
 
 PKG := $(subst $(GO_PATH)/src/,,$(CURDIR))
 GO_PKGS := $(shell go list ./... | grep -v -e '.pb.go')
-GO_PKGS_ABS := $(shell go list -f '$(GO_PATH)/src/{{.ImportPath}}' ./... | grep -v -e '.pb.go')
+GO_APP_PKGS := $(shell go list -f '{{if and (or .GoFiles .CgoFiles) (ne .Name "main")}}{{.ImportPath}}{{end}}' ${PKG}/...)
 GO_TEST_PKGS := $(shell go list -f='{{if or .TestGoFiles .XTestGoFiles}}{{.ImportPath}}{{end}}' ./...)
-GO_VENDOR_PKGS := $(shell go list -f '{{if and (or .GoFiles .CgoFiles) (ne .Name "main")}}./vendor/{{.ImportPath}}{{end}}' ./vendor/...)
-
-VERSION := $(shell cat VERSION.txt)
-GIT_COMMIT := $(shell git rev-parse --short HEAD)
-GIT_UNTRACKED_CHANGES:= $(shell git status --porcelain --untracked-files=no)
-ifneq ($(GIT_UNTRACKED_CHANGES),)
-	GIT_COMMIT := $(GIT_COMMIT)-dirty
-endif
-CTIMEVAR:=-X main.version=$(VERSION) -X main.gitCommit=$(GIT_COMMIT)
-
-CGO_ENABLED ?= 0
-GO_LDFLAGS:=-ldflags "-w $(CTIMEVAR)"
-GO_LDFLAGS_STATIC:=-ldflags "-w $(CTIMEVAR) -extldflags -static"
-GO_BUILD_TAGS := osusergo
-GO_FLAGS ?= -tags '$(GO_BUILD_TAGS)'
-
-ifneq ($(wildcard go.mod),)  # exist go.mod file
-ifeq ($(CI),)  # $CI is empty
-	GO_FLAGS+=-mod=vendor
-else
-	GO_FLAGS+=-mod=readonly
-endif
+GO_VENDOR_PKGS=
+ifneq ($(wildcard ./vendor),)
+	GO_VENDOR_PKGS = $(shell go list -f '{{if and (or .GoFiles .CgoFiles) (ne .Name "main")}}./vendor/{{.ImportPath}}{{end}}' ./vendor/...)
 endif
 
 GO_TEST ?= go test
@@ -38,14 +24,61 @@ GO_TEST_FLAGS ?=
 GO_BENCH_FUNC ?= .
 GO_BENCH_FLAGS ?= -benchmem
 
-IMAGE_REGISTRY := gcr.io/container-image
+VERSION := $(shell cat VERSION.txt)
+GIT_COMMIT := $(shell git rev-parse --short HEAD)
+GIT_UNTRACKED_CHANGES := $(shell git status --porcelain --untracked-files=no)
+# disable '-dirty' suffix if $CLOUD_BUILD, $CIRCLECI are not empty
+ifeq (${CIRCLECI},)
+ifneq (${GIT_UNTRACKED_CHANGES},)
+	GIT_COMMIT := $(GIT_COMMIT)-dirty
+endif
+endif
+CTIMEVAR := -X main.version=v$(VERSION) -X main.gitCommit=$(GIT_COMMIT)
+
+CGO_ENABLED ?= 0
+GO_LDFLAGS=-s -w $(CTIMEVAR)
+GO_LDFLAGS_STATIC=-s -w '-extldflags=-static' $(CTIMEVAR)
+
+# disable '-d' ldflags if $CIRCLECI is not empty, or ($GO_OS != darwin)
+ifeq (${CIRCLECI},)
+ifneq (${GO_OS},darwin)
+	GO_LDFLAGS_STATIC+=-d
+endif
+endif
+
+GO_BUILDTAGS := osusergo netgo
+GO_BUILDTAGS_STATIC=static static_build
+GO_FLAGS ?= -tags='$(GO_BUILDTAGS)' -ldflags="${GO_LDFLAGS}"
+GO_INSTALLSUFFIX_STATIC=netgo
+
+ifneq ($(wildcard go.mod),)  # exist go.mod
+ifeq ($(GO111MODULE),on)
+	GO_FLAGS+=-mod=vendor
+endif
+endif
+
+CONTAINER_REGISTRY := gcr.io/container-image/microservice-platform
+CONTAINER_BUILD_TAG ?= $(VERSION)
+CONTAINER_BUILD_ARGS_BASE = --rm --pull --build-arg GOLANG_VERSION=${GOLANG_VERSION} --build-arg ALPINE_VERSION=${ALPINE_VERSION}
+ifneq (${SHORT_SHA},)
+	CONTAINER_BUILD_ARGS_BASE+=--build-arg SHORT_SHA=${SHORT_SHA}
+endif
+CONTINUOUS_INTEGRATION ?=
+ifneq (${CONTINUOUS_INTEGRATION},)
+	CONTAINER_BUILD_ARGS_BASE+=--progress=plain
+endif
+CONTAINER_BUILD_ARGS ?= ${CONTAINER_BUILD_ARGS_BASE}
+CONTAINER_BUILD_TARGET ?= ${APP}
+ifneq (${CONTAINER_BUILD_TARGET},${APP})
+	CONTAINER_BUILD_TAG=${VERSION}-${CONTAINER_BUILD_TARGET}
+endif
 
 # ----------------------------------------------------------------------------
 # defines
 
 GOPHER = ""
 define target
-@printf "$(GOPHER)  \\033[32m$(patsubst ,$@,$(1))\\033[0m\\n"
+@printf "$(GOPHER)  \\x1b[1;32m$(patsubst ,$@,$(1))\\x1b[0m\\n"
 endef
 
 # ----------------------------------------------------------------------------
@@ -56,22 +89,32 @@ endef
 .PHONY: $(APP)
 $(APP): VERSION.txt
 	$(call target)
-	CGO_ENABLED=$(CGO_ENABLED) go build -v $(strip $(GO_FLAGS)) -o $(APP) $(CMD)
+	CGO_ENABLED=$(CGO_ENABLED) GOOS=$(GO_OS) GOARCH=$(GO_ARCH) go build -v $(strip $(GO_FLAGS)) -o $(APP) $(CMD)
 
 .PHONY: build
-build: GO_FLAGS+=${GO_LDFLAGS}
 build: $(APP)  ## Builds a dynamic executable or package.
 
 .PHONY: static
-static: GO_FLAGS+=${GO_LDFLAGS_STATIC}
-static: GO_BUILD_TAGS+=static
+static: GO_LDFLAGS=${GO_LDFLAGS_STATIC}
+static: GO_FLAGS+=-a -installsuffix ${GO_INSTALLSUFFIX_STATIC}
+static: GO_BUILDTAGS+=${GO_BUILDTAGS_STATIC}
 static: $(APP)  ## Builds a static executable or package.
 
 .PHONY: install
-install: GO_FLAGS+=${GO_LDFLAGS_STATIC}
+install: GO_LDFLAGS=${GO_LDFLAGS_STATIC}
+install: GO_FLAGS+=-a -installsuffix ${GO_INSTALLSUFFIX_STATIC}
+install: GO_BUILDTAGS+=${GO_BUILDTAGS_STATIC}
 install:  ## Installs the executable or package.
 	$(call target)
-	CGO_ENABLED=$(CGO_ENABLED) go install -a -v $(strip $(GO_FLAGS)) $(CMD)
+	CGO_ENABLED=$(CGO_ENABLED) GOOS=$(GO_OS) GOARCH=$(GO_ARCH) go install -v $(strip $(GO_FLAGS)) $(CMD)
+
+.PHONY: pkg/install
+pkg/install: GO_LDFLAGS=${GO_LDFLAGS_STATIC}
+pkg/install: GO_FLAGS+=-installsuffix ${GO_INSTALLSUFFIX_STATIC}
+pkg/install: GO_BUILDTAGS+=${GO_BUILDTAGS_STATIC}
+pkg/install:
+	$(call target)
+	CGO_ENABLED=$(CGO_ENABLED) GOOS=$(GO_OS) GOARCH=$(GO_ARCH) go install -v ${GO_APP_PKGS}
 
 
 ## test, bench and coverage
@@ -107,17 +150,21 @@ $(GO_PATH)/bin/go-junit-report:
 .PHONY: cmd/go-junit-report
 cmd/go-junit-report: $(GO_PATH)/bin/go-junit-report  # go get 'go-junit-report' binary
 
-.PHONY: coverage/junit
-coverage/junit: cmd/go-junit-report  ## Take test coverage and output test results with junit syntax.
+.PHONY: coverage/ci
+coverage/ci: GO_LDFLAGS=${GO_LDFLAGS_STATIC}
+coverage/ci: GO_FLAGS+=-a -installsuffix ${GO_INSTALLSUFFIX_STATIC}
+coverage/ci: GO_BUILDTAGS+=${GO_BUILDTAGS_STATIC}
+coverage/ci: cmd/go-junit-report  ## Take test coverage.
 	$(call target)
-	@mkdir -p test-results
-	$(GO_TEST) -v -race $(strip $(GO_FLAGS)) -covermode=atomic -coverpkg=$(PKG)/... -coverprofile=coverage.out $(GO_PKGS) 2>&1 | tee /dev/stderr | go-junit-report -set-exit-code > test-results/report.xml
+	@mkdir -p /tmp/ci/artifacts /tmp/ci/test-results
+	$(GO_TEST) -v -race $(strip $(GO_FLAGS)) -covermode=atomic -coverpkg=$(PKG)/... -coverprofile=/tmp/ci/artifacts/coverage.out $(GO_PKGS) 2>&1 | tee /dev/stderr | go-junit-report -set-exit-code > /tmp/ci/test-results/junit.xml
+	@go tool cover -html=/tmp/ci/artifacts/coverage.out -o /tmp/ci/artifacts/coverage.html
 
 
 ## lint
 
 .PHONY: lint
-lint: lint/fmt lint/govet lint/golangci-lint  ## Run all linters.
+lint: lint/golangci-lint  ## Run all linters.
 
 .PHONY: lint/fmt
 lint/fmt:  ## Verifies all files have been `gofmt`ed.
@@ -175,6 +222,8 @@ mod/clean:
 	@$(RM) -r vendor
 
 .PHONY: mod/install
+mod/install: GO_LDFLAGS=${GO_LDFLAGS_STATIC}
+mod/install: GO_FLAGS+=-installsuffix ${GO_INSTALLSUFFIX_STATIC}
 mod/install: mod/tidy mod/vendor
 	$(call target)
 	@GO111MODULE=off go install -v $(GO_VENDOR_PKGS) || GO111MODULE=on go install -mod=vendor -v $(GO_VENDOR_PKGS)
@@ -190,12 +239,12 @@ mod/update: mod/goget mod/tidy mod/vendor mod/install  ## Updates all vendor pac
 ## miscellaneous
 
 .PHONY: container/build
-container/build: Dockerfile  ## Create the container image from the Dockerfile.
-	docker image build --rm --force-rm --pull --progress=plain -t $(IMAGE_REGISTRY)/$(APP):$(VERSION:v%=%) .
+container/build:  ## Create the container image from the Dockerfile.
+	docker image build ${CONTAINER_BUILD_ARGS} --target ${CONTAINER_BUILD_TARGET} -t $(CONTAINER_REGISTRY)/$(APP):${CONTAINER_BUILD_TAG} .
 
 .PHONY: container/push
-container/push:  ## Push the container image to $IMAGE_REGISTRY.
-	docker image push $(IMAGE_REGISTRY)/$(APP):$(VERSION:v%=%)
+container/push:  ## Push the container image to $CONTAINER_REGISTRY
+	docker image push $(CONTAINER_REGISTRY)/$(APP):$(VERSION)
 
 
 .PHONY: boilerplate/go/%
